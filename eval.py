3f55ae7e249ed8f5bf478d7b26918ea37c9b45e3
lzyjo <lmzyjoey@gmail.com>
2024-12-03 17:46:46 +0000

    set checkpoint to separate cell, and remove checkpoint = ... from eval.py

--- eval.py
+++ eval.py
@@ -12,7 +12,6 @@ keep_difficult = True  # difficult ground truth objects must always be considere
 batch_size = 64
 workers = 4
 device = torch.device("cuda")
-checkpoint = './checkpoint_ssd300.pth.tar'
 
 # Load model checkpoint that is to be evaluated
 checkpoint = torch.load(checkpoint)
